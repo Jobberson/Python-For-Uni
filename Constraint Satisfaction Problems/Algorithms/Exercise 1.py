f243ed82540875d0a0f4fb146ffe001dfdbd77df
Pedro Schenegoski <skygames410@gmail.com>
2025-05-06 21:11:11 -0300

    Added CSP folders and the simple exercise
    I added the folders and i made the simple exercise as well as setting up for the next exercise

---
+++ Constraint Satisfaction Problems/Algorithms/Exercise 1.py
@@ -0,0 +1,10 @@
+# Você deve planejar a irrigação de uma plantação por 5 dias consecutivos. A irrigação total não pode ultrapassar 20 mm.
+#
+# Previsão do Tempo:
+# ● Dia 1: Sem chuva (irrigar 10 mm, 5 mm, ou 2 mm)
+# ● Dia 2: Chuva leve (0 a 4 mm de chuva prevista - irrigar 5 mm ou 2 mm)
+# ● Dia 3: Chuva moderada (5 a 9 mm de chuva prevista - irrigar 2 mm ou 0 mm)
+# ● Dia 4: Sem chuva (irrigar 10 mm, 5 mm, ou 2 mm)
+# ● Dia 5: Chuva intensa (10 mm ou mais de chuva prevista - irrigar 0 mm)
+#
+# Encontre uma combinação válida.
---
+++ Constraint Satisfaction Problems/Algorithms/Exercise 2.py
@@ -0,0 +1,9 @@
+# Cinco amigos - Ana, Bruno, Carlos, Diana e Eduardo - estão sentados em uma mesa redonda. Eles
+# precisam se sentar de forma que:
+#
+# 1. Ana não pode estar ao lado de Bruno nem de Carlos.
+# 2. Diana não pode estar ao lado de Eduardo.
+# 3. Carlos não pode estar ao lado de Eduardo.
+#
+# Encontre uma configuração válida onde todos os amigos estejam sentados na mesa de forma que essas
+# restrições sejam respeitadas.
